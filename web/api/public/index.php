6c30ee96e350b252e6fac7e1ee598026c7b30448
brandonkelly <brandon@pixelandtonic.com>
2017-05-31 16:22:15 -0700

    Initial structure

---
+++ web/api/public/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Craft web bootstrap file
+ */
+
+define('CRAFT_BASE_PATH', dirname(__DIR__));
+define('CRAFT_VENDOR_PATH', dirname(CRAFT_BASE_PATH, 2).'/vendor');
+
+// Composer autoloader
+require_once CRAFT_VENDOR_PATH.'/autoload.php';
+
+// dotenv
+$dotenv = new Dotenv\Dotenv(dirname(CRAFT_VENDOR_PATH));
+$dotenv->load();
+
+$app = require CRAFT_VENDOR_PATH.'/craftcms/cms/bootstrap/web.php';
+$app->run();
