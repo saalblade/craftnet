2b8e38c91d0002f6ec276afc7a4594b7bf722c0b
Brad Bell <brad@pixelandtonic.com>
2017-07-14 14:07:08 -0700

    config cleanup.

--- web/api/config/general.php
+++ web/api/config/general.php
@@ -10,18 +10,12 @@ return [
     // Base site URL
     'siteUrl' => null,
 
-    // Default Week Start Day (0 = Sunday, 1 = Monday...)
-    'defaultWeekStartDay' => 0,
-
-    // Enable CSRF Protection (recommended, will be enabled by default in Craft 3)
-    'enableCsrfProtection' => true,
-
     // Whether "index.php" should be visible in URLs (true, false, "auto")
     'omitScriptNameInUrls' => true,
 
     // Control Panel trigger word
-    'cpTrigger' => 'admin',
+    'cpTrigger' => 'ramos',
 
     // Dev Mode (see https://craftcms.com/support/dev-mode)
-    'devMode' => false,
+    'devMode' => true,
 ];
