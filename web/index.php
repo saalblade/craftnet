b83e1bb290122a8f526c47396ed68eb954ac75b7
Brad Bell <brad@pixelandtonic.com>
2017-07-24 16:17:25 -0700

    craft.cm

--- web/index.php
+++ web/index.php
@@ -4,16 +4,19 @@
  */
 
 switch($_SERVER['HTTP_HOST']) {
-    case 'api.craftcms.com':
+    case 'api.craft.com':
     case 'api.craftcms.dev':
+    case 'api.craft.cm':
         define('CRAFT_SITE', 'api');
         break;
     case 'composer.craftcms.com':
     case 'composer.craftcms.dev':
+    case 'composer.craft.cm':
         define('CRAFT_SITE', 'composer');
         break;
     case 'id.craftcms.com':
     case 'id.craftcms.dev':
+    case 'id.craft.cm':
         define('CRAFT_SITE', 'craftId');
         break;
 }
