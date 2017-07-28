d61ed32df00e0e60b34019d81f60bc8fb0d9e8fe
Brad Bell <brad@pixelandtonic.com>
2017-07-28 16:02:49 -0700

    Redis for cache and session.

--- config/app.php
+++ config/app.php
@@ -4,4 +4,23 @@ return [
     'modules' => [
         'api' => \craftcom\api\Module::class
     ],
+    'components' => [
+        'redis' => [
+            'class' => 'yii\redis\Connection',
+            'hostname' => 'craft.4qveoj.ng.0001.usw2.cache.amazonaws.com',
+            'port' => 6379,
+            'database' => 0,
+        ],
+        'cache' => [
+            'class' => 'yii\redis\Cache',
+            'redis' => [
+                'hostname' => 'craft.4qveoj.ng.0001.usw2.cache.amazonaws.com',
+                'port' => 6379,
+                'database' => 0,
+            ],
+        ],
+        'session' => [
+            'class' => 'yii\redis\Session',
+        ],
+    ]
 ];
