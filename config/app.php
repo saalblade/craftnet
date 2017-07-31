00e707fe2e027ecbaf13558825155b569287fe33
brandonkelly <brandon@pixelandtonic.com>
2017-07-31 16:55:26 -0700

    Only use Redis on production

--- config/app.php
+++ config/app.php
@@ -1,40 +1,42 @@
 <?php
 
-use yii\redis\Session;
-
 return [
-    'modules' => [
-        'api' => \craftcom\api\Module::class
-    ],
-    'components' => [
-        'redis' => [
-            'class' => 'yii\redis\Connection',
-            'hostname' => 'craft.4qveoj.ng.0001.usw2.cache.amazonaws.com',
-            'port' => 6379,
-            'database' => 0,
+    '*' => [
+        'modules' => [
+            'api' => \craftcom\api\Module::class
         ],
-        'cache' => [
-            'class' => 'yii\redis\Cache',
+    ],
+    '.com' => [
+        'components' => [
             'redis' => [
+                'class' => 'yii\redis\Connection',
                 'hostname' => 'craft.4qveoj.ng.0001.usw2.cache.amazonaws.com',
                 'port' => 6379,
                 'database' => 0,
             ],
+            'cache' => [
+                'class' => 'yii\redis\Cache',
+                'redis' => [
+                    'hostname' => 'craft.4qveoj.ng.0001.usw2.cache.amazonaws.com',
+                    'port' => 6379,
+                    'database' => 0,
+                ],
+            ],
+            'session' => function() {
+                $stateKeyPrefix = md5('Craft.'.craft\web\Session::class.'.'.Craft::$app->id);
+
+                /** @var yii\redis\Session $session */
+                $session = Craft::createObject([
+                    'class' => yii\redis\Session::class,
+                    'flashParam' => $stateKeyPrefix.'__flash',
+                    'name' => Craft::$app->getConfig()->getGeneral()->phpSessionName,
+                    'cookieParams' => Craft::cookieConfig(),
+                ]);
+
+                $session->attachBehaviors([craft\behaviors\SessionBehavior::class]);
+                $session->authAccessParam = $stateKeyPrefix.'__auth_access';
+                return $session;
+            },
         ],
-        'session' => function() {
-            $stateKeyPrefix = md5('Craft.'.craft\web\Session::class.'.'.Craft::$app->id);
-
-            /** @var Session $session */
-            $session = Craft::createObject([
-                'class' => Session::class,
-                'flashParam' => $stateKeyPrefix.'__flash',
-                'name' => Craft::$app->getConfig()->getGeneral()->phpSessionName,
-                'cookieParams' => Craft::cookieConfig(),
-            ]);
-
-            $session->attachBehaviors([craft\behaviors\SessionBehavior::class]);
-            $session->authAccessParam = $stateKeyPrefix.'__auth_access';
-            return $session;
-        },
     ]
 ];
