191385c6f868e8b75ff52b6f7202db87ba1b1ff7
Benjamin David <ben@dukt.net>
2017-09-14 02:52:15 +0200

    Refactoring

--- web/vue/resources/assets/js/store/index.js
+++ web/vue/resources/assets/js/store/index.js
@@ -3,6 +3,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex);
+
 export default new Vuex.Store({
     strict: true,
 
@@ -12,65 +13,79 @@ export default new Vuex.Store({
         stripeCustomer: null,
         stripeCard: null,
     },
+
     getters: {
         craftId: state => {
             return state.craftId;
         },
+
         stripeAccount: state => {
             return state.stripeAccount;
         },
+
         stripeCard: state => {
             return state.stripeCard;
         },
+
         stripeCustomer: state => {
             return state.stripeCustomer;
         },
+
         craftLicenses: state => {
             if(state.craftId) {
                 return state.craftId.craftLicenses;
             }
         },
+
         currentUser: state => {
             if(state.craftId) {
                 return state.craftId.currentUser;
             }
         },
+
         customers: state => {
             if(state.craftId) {
                 return state.craftId.customers;
             }
         },
+
         licenses: state => {
             if(state.craftId) {
                 return state.craftId.pluginLicenses.concat(state.craftId.craftLicenses);
             }
         },
+
         payments: state => {
             if(state.craftId) {
                 return state.craftId.payments;
             }
         },
+
         payouts: state => {
             if(state.craftId) {
                 return state.craftId.payouts;
             }
         },
+
         payoutsScheduled: state => {
             if(state.craftId) {
                 return state.craftId.payoutsScheduled;
             }
         },
+
         pluginLicenses: state => {
             if(state.craftId) {
                 return state.craftId.pluginLicenses;
             }
         },
+
         plugins: state => {
             if(state.craftId) {
                 return state.craftId.plugins;
             }
         },
     },
+
     actions: {
         getCraftIdData ({ commit }) {
             return new Promise((resolve, reject) => {
