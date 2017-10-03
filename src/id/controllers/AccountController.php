b8f36405f79509a075fdc158d49ced2ea2b3c1fe
Benjamin David <ben@dukt.net>
2017-10-03 03:14:00 +0200

    Added `craftcom\id\controllers\AccountController::actionUploadUserPhoto()`

--- src/id/controllers/AccountController.php
+++ src/id/controllers/AccountController.php
@@ -33,4 +33,56 @@ class AccountController extends Controller
             'stripeAccessToken' => $stripeAccessToken
         ]);
     }
+
+    /**
+     * Upload a user photo.
+     *
+     * @return Response|null
+     * @throws BadRequestHttpException if the uploaded file is not an image
+     */
+    public function actionUploadUserPhoto()
+    {
+        $this->requireAcceptsJson();
+        $this->requireLogin();
+        $userId = Craft::$app->getRequest()->getRequiredBodyParam('userId');
+
+        if ($userId !== Craft::$app->getUser()->getIdentity()->id) {
+            $this->requirePermission('editUsers');
+        }
+
+        if (($file = UploadedFile::getInstanceByName('photo')) === null) {
+            return null;
+        }
+
+        try {
+            if ($file->getHasError()) {
+                throw new UploadFailedException($file->error);
+            }
+
+            $users = Craft::$app->getUsers();
+            $user = $users->getUserById($userId);
+
+            // Move to our own temp location
+            $fileLocation = Assets::tempFilePath($file->getExtension());
+            move_uploaded_file($file->tempName, $fileLocation);
+            $users->saveUserPhoto($fileLocation, $user, $file->name);
+
+            return $this->asJson([
+                'photoId' => $user->photoId,
+                'photoUrl' => $user->getThumbUrl(200),
+            ]);
+        } catch (\Throwable $exception) {
+            /** @noinspection UnSafeIsSetOverArrayInspection - FP */
+            if (isset($fileLocation)) {
+                FileHelper::removeFile($fileLocation);
+            }
+
+            Craft::error('There was an error uploading the photo: '.$exception->getMessage(), __METHOD__);
+
+            return $this->asErrorJson(Craft::t('app', 'There was an error uploading your photo: {error}', [
+                'error' => $exception->getMessage()
+            ]));
+        }
+    }
+
 }
