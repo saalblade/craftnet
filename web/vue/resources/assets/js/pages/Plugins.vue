f9d8112c8e63594374f55afe71d6070be1c292ee
Benjamin David <ben@dukt.net>
2017-10-09 11:45:12 +0200

    Renamed “Submit” link to “Add a plugin”

--- web/vue/resources/assets/js/pages/Plugins.vue
+++ web/vue/resources/assets/js/pages/Plugins.vue
@@ -3,7 +3,7 @@
 
         <ul class="nav nav-pills mb-4">
             <li class="nav-item"><router-link class="nav-link" to="/developer/plugins" exact>Your Plugins</router-link></li>
-            <li class="nav-item"><router-link class="nav-link" to="/developer/plugins/submit">Submit</router-link></li>
+            <li class="nav-item"><router-link class="nav-link" to="/developer/plugins/submit">Add a plugin</router-link></li>
         </ul>
 
         <router-view :key="$route.fullPath"></router-view>
