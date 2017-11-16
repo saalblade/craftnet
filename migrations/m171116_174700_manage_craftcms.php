8a600a9813dc7c599d12ffd209e585889be26312
brandonkelly <brandon@pixelandtonic.com>
2017-11-16 11:54:21 -0800

    Make sure craftcms/cms is managed

---
+++ migrations/m171116_174700_manage_craftcms.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace craft\contentmigrations;
+
+use Craft;
+use craft\db\Migration;
+
+/**
+ * m171116_174700_manage_craftcms migration.
+ */
+class m171116_174700_manage_craftcms extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $this->update('craftcom_packages', [
+            'repository' => 'https://github.com/craftcms/cms',
+            'managed' => true,
+        ], [
+            'name' => 'craftcms/cms',
+        ], [], false);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m171116_174700_manage_craftcms cannot be reverted.\n";
+        return false;
+    }
+}
