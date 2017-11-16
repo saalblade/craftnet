eff5592ca7840dc12d61145ca400f5b2f8822dd0
brandonkelly <brandon@pixelandtonic.com>
2017-11-16 12:25:29 -0800

    Update all plugins (for changelogs)

---
+++ migrations/m171116_202033_update_all_plugins.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace craft\contentmigrations;
+
+use Craft;
+use craft\db\Migration;
+use craft\db\Query;
+use craftcom\composer\jobs\UpdatePackage;
+
+/**
+ * m171116_202033_update_all_plugins migration.
+ */
+class m171116_202033_update_all_plugins extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $names = (new Query())
+            ->select(['packageName'])
+            ->from(['craftcom_plugins'])
+            ->where(['not', ['changelogPath' => null]])
+            ->column($this->db);
+
+        $queue = Craft::$app->getQueue();
+
+        foreach ($names as $name) {
+            $queue->push(new UpdatePackage([
+                'name' => $name,
+                'force' => true,
+                'skipIfRecentlyUpdated' => false,
+            ]));
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m171116_202033_update_all_plugins cannot be reverted.\n";
+        return false;
+    }
+}
