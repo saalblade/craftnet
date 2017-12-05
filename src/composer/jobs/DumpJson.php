bd2493983b36bb2168e95b0dbedbcf9be1907f91
Brad Bell <brad@pixelandtonic.com>
2017-12-05 11:02:53 -0800

    DumpJson logging.

--- src/composer/jobs/DumpJson.php
+++ src/composer/jobs/DumpJson.php
@@ -4,11 +4,13 @@ namespace craftcom\composer\jobs;
 
 use craft\queue\BaseJob;
 use craftcom\Module;
+use Craft;
 
 class DumpJson extends BaseJob
 {
     public function execute($queue)
     {
+        Craft::info('Executing DumpJson job.', __METHOD__);
         Module::getInstance()->getJsonDumper()->dump();
     }
 
