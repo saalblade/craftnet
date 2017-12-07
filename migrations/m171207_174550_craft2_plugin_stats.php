f6afbb316de5dcbd7624c840f80005765418baa0
brandonkelly <brandon@pixelandtonic.com>
2017-12-07 09:58:28 -0800

    craft2pluginhits table

---
+++ migrations/m171207_174550_craft2_plugin_stats.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace craft\contentmigrations;
+
+use Craft;
+use craft\db\Migration;
+
+/**
+ * m171207_174550_craft2_plugin_stats migration.
+ */
+class m171207_174550_craft2_plugin_stats extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $this->createTable('craftcom_craft2pluginhits', [
+            'plugin' => $this->string(),
+            'hits' => $this->integer(),
+            'PRIMARY KEY(plugin)',
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m171207_174550_craft2_plugin_stats cannot be reverted.\n";
+        return false;
+    }
+}
