b7b402544add7742ec07e0cd5051a6ed93d259e0
brandonkelly <brandon@pixelandtonic.com>
2017-12-08 09:55:54 -0800

    totalInstalls => 0

--- src/controllers/api/BaseApiController.php
+++ src/controllers/api/BaseApiController.php
@@ -198,6 +198,7 @@ abstract class BaseApiController extends Controller
             'categoryIds' => ArrayHelper::getColumn($plugin->getCategories(), 'id'),
             'version' => $plugin->latestVersion,
             'packageName' => $plugin->packageName,
+            'totalInstalls' => 0,
         ];
 
         if ($fullDetails) {
