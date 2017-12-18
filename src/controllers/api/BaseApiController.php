2be5de78ec1ff3934730e9cb443d785aa28e9d09
brandonkelly <brandon@pixelandtonic.com>
2017-12-18 07:36:00 -0800

    keywords

--- src/controllers/api/BaseApiController.php
+++ src/controllers/api/BaseApiController.php
@@ -199,6 +199,7 @@ abstract class BaseApiController extends Controller
             'developerId' => $developer->id,
             'developerName' => strip_tags($developer->getDeveloperName()),
             'categoryIds' => ArrayHelper::getColumn($plugin->getCategories(), 'id'),
+            'keywords' => [],
             'version' => $plugin->latestVersion,
             'activeInstalls' => $plugin->activeInstalls,
             'packageName' => $plugin->packageName,
