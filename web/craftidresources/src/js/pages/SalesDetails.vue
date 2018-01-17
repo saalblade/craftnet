169e1916fd6a78e2ebc4b65ff0f93abc44170cd7
Benjamin David <ben@dukt.net>
2018-01-17 13:04:06 +0100

    Improved SalesDetails

--- web/craftidresources/src/js/pages/SalesDetails.vue
+++ web/craftidresources/src/js/pages/SalesDetails.vue
@@ -1,26 +1,36 @@
 <template>
     <div v-if="sale">
+        <p><router-link class="nav-link" to="/developer/sales" exact>← Back to sales</router-link></p>
+
         <div class="card mb-3">
             <div class="card-body">
                 <h3>{{ sale.amount|currency }}</h3>
-                <p class="text-secondary">#00000{{sale.id}}</p>
+                <p class="text-secondary">#SAL000{{sale.id}}</p>
 
                 <hr>
 
-                <dl>
-                    <dt>Item</dt>
-                    <dd>{{ sale.plugin.name }}</dd>
-                    <dt>Type</dt>
-                    <dd>{{ sale.type }}</dd>
-                    <dt>Amount</dt>
-                    <dd>{{ sale.amount|currency }}</dd>
-                    <dt>Payment ID</dt>
-                    <dd>#{{ sale.id }}</dd>
-                    <dt>Customer</dt>
-                    <dd><router-link :to="'/developer/customers/'+sale.customer.id">{{ sale.customer.email }}</router-link></dd>
-                    <dt>Date</dt>
-                    <dd>{{ sale.date }}</dd>
-                </dl>
+                <div class="row">
+                    <div class="col-6">
+                        <dl>
+                            <dt>Item</dt>
+                            <dd>{{ sale.plugin.name }}</dd>
+                            <dt>Type</dt>
+                            <dd>{{ sale.type }}</dd>
+                            <dt>Amount</dt>
+                            <dd>{{ sale.amount|currency }}</dd>
+                        </dl>
+                    </div>
+                    <div class="col-6">
+                        <dl>
+                            <dt>Sale ID</dt>
+                            <dd>#{{ sale.id }}</dd>
+                            <dt>Customer</dt>
+                            <dd><router-link :to="'/developer/customers/'+sale.customer.id">{{ sale.customer.email }}</router-link></dd>
+                            <dt>Date</dt>
+                            <dd>{{ sale.date }}</dd>
+                        </dl>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
