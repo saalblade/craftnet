19addac718f07cc00737f85f0facc1ca44a73b06
Benjamin David <ben@dukt.net>
2018-02-02 15:34:57 +0100

    Added LightSwitchInput

---
+++ web/craftidresources/src/js/components/inputs/LightswitchInput.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<label for="t1" class="lightswitch">
+			<input id="t1" type="checkbox" :checked="value" @change="$emit('input', $event.target.checked)" />
+			<div class="slider round"></div>
+		</label>
+	</div>
+</template>
+
+<script>
+    export default {
+
+        props: ['value'],
+
+		methods: {
+            turnOn() {
+                console.log('turn on');
+			}
+		}
+
+    }
+
+</script>
