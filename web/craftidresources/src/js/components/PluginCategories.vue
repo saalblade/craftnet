baf4612dcdb1ec51854a549ac220469e6cae2d8e
Benjamin David <ben@dukt.net>
2018-02-06 17:09:17 +0100

    Improved PluginCategories

--- web/craftidresources/src/js/components/PluginCategories.vue
+++ web/craftidresources/src/js/components/PluginCategories.vue
@@ -5,11 +5,11 @@
 			<div class="instructions"><p>Pick up to {{maxCategories}} categories. ({{ pluginDraft.categoryIds.length }}/{{maxCategories}} selected)</p></div>
 
 			<draggable v-model="pluginDraft.categoryIds">
-				<div class="alert float-left clearfix mb-3" v-for="category in selectedCategories">
+				<div class="alert float-left clearfix mb-3 mr-2 px-3 py-2" v-for="category in selectedCategories">
 					<div class="flex">
 						<div>{{category.title}}</div>
 						<div class="ml-3 mt-1">
-							<a class="" href="#" @click.prevent="unselectCategory(category.id)"><i class="fa fa-remove text-red"></i></a>
+							<a class="" href="#" @click.prevent="unselectCategory(category.id)"><i class="fas fa-times text-red"></i></a>
 						</div>
 					</div>
 				</div>
@@ -19,7 +19,7 @@
 
 			<div>
 				<div class="inline-block" v-for="category in availableCategories">
-					<a class="btn btn-outline-secondary mb-2" :class="{disabled: pluginDraft.categoryIds.length >= maxCategories }" href="#" @click.prevent="selectCategory(category.id)"><i class="fa fa-plus"></i> {{category.title}}</a>&nbsp;
+					<a class="btn btn-outline-secondary mb-2 mr-2" :class="{disabled: pluginDraft.categoryIds.length >= maxCategories }" href="#" @click.prevent="selectCategory(category.id)"><i class="fa fa-plus"></i> {{category.title}}</a>
 				</div>
 			</div>
 		</div>
