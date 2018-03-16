1db72ffd5846c576989d759260840d1f3d3396d5
brandonkelly <brandon@pixelandtonic.com>
2018-03-16 13:52:49 -0700

    Better dev funds management

--- src/developers/FundsManager.php
+++ src/developers/FundsManager.php
@@ -3,9 +3,13 @@
 namespace craftcom\developers;
 
 use Craft;
-use craft\commerce\elements\Order;
+use craft\db\Query;
 use craft\elements\User;
 use craft\helpers\Db;
+use craftcom\errors\InaccessibleFundsException;
+use craftcom\errors\InsufficientFundsException;
+use craftcom\errors\MissingStripeAccountException;
+use Stripe\Error\Base as StripeError;
 use Stripe\Stripe;
 use Stripe\Transfer;
 use yii\base\BaseObject;
@@ -20,24 +24,48 @@ class FundsManager extends BaseObject
     public $developer;
 
     /**
+     * @var string
+     */
+    private $_lockName;
+
+    /**
+     * @var bool
+     * @see _lockFunds()
+     * @see _unlockFunds()
+     */
+    private $_lockLevel = 0;
+
+    /**
      * @inheritdoc
      */
     public function __construct(User $developer, array $config = [])
     {
         $this->developer = $developer;
+        $this->_lockName = 'funds:'.$developer->id;
         parent::__construct($config);
     }
 
     /**
-     * Credits a developer's account.
+     * Returns the developer’s current balance.
+     */
+    public function getBalance(): float
+    {
+        return (new Query())
+            ->select(['balance'])
+            ->from('craftcom_developers')
+            ->where(['id' => $this->developer->id])
+            ->scalar();
+    }
+
+    /**
+     * Credits the developer's account.
      *
      * @param string $note the transaction note
      * @param float $credit the credit amount (not including any fees that need to be removed)
      * @param float|null $fee the total fees that should be deducted from the credit amount
      * @throws InvalidArgumentException if $credit or $fee are negative or 0
-     * @return $this
      */
-    public function credit(string $note, float $credit, float $fee = null): self
+    public function credit(string $note, float $credit, float $fee = null)
     {
         if ($credit <= 0) {
             throw new InvalidArgumentException('Invalid credit amount: '.$credit);
@@ -48,54 +76,29 @@ class FundsManager extends BaseObject
         }
 
         $this->_addTransaction($note, $credit, null, $fee);
-        return $this;
     }
 
     /**
-     * Debits a developer's account.
+     * Debits the developer's account.
      *
      * @param string $note the transaction note
      * @param float $debit the credit amount (not including any fees that need to be removed)
      * @throws InvalidArgumentException if $debit is negative or 0
-     * @return $this
+     * @throws InaccessibleFundsException if the developer's funds could not be locked
      */
-    public function debit(string $note, float $debit): self
+    public function debit(string $note, float $debit)
     {
         if ($debit <= 0) {
             throw new InvalidArgumentException('Invalid debit amount: '.$debit);
         }
 
-        $this->_addTransaction($note, null, $debit);
-        return $this;
-    }
-
-    /**
-     * Attempts to issue a payout to the developer.
-     *
-     * @param string $note the transaction note
-     * @param float $amount the payout amount
-     * @throws InvalidArgumentException if $amount is negative or 0
-     * @return $this
-     */
-    public function transfer(string $note, float $amount): self
-    {
         // no double-dipping
-        $mutex = Craft::$app->getMutex();
-        $lockName = 'funds:'.$this->developer->id;
-        if (!$mutex->acquire($lockName, 1)) {
-            Craft::warning("Aborted funds transfer for {$this->developer->id}: could not acquire lock on funds", __METHOD__);
-            return $this;
+        if (!$this->_lockFunds()) {
+            throw new InaccessibleFundsException();
         }
 
-        // attempt to transfer the funds
-        if ($this->_createTransfer($amount)) {
-            $this->debit($note, $amount);
-        };
-
-        // unlock the funds
-        $mutex->release($lockName);
-
-        return $this;
+        $this->_addTransaction($note, null, $debit);
+        $this->_unlockFunds();
     }
 
     /**
@@ -107,34 +110,129 @@ class FundsManager extends BaseObject
      * @param float $credit the credit amount (not including any fees that need to be removed)
      * @param float|null $fee the total fees that should be deducted from the credit amount
      * @throws InvalidArgumentException if $credit or $fee are negative or 0
-     * @return $this
      */
-    public function processOrder(string $orderNumber, string $chargeId, float $credit, float $fee = null): self
+    public function processOrder(string $orderNumber, string $chargeId, float $credit, float $fee = null)
     {
+        $e = null;
+
         // no double-dipping
-        $mutex = Craft::$app->getMutex();
-        $lockName = 'funds:'.$this->developer->id;
-        $locked = $mutex->acquire($lockName, 1);
+        if (!$this->_lockFunds(5)) {
+            $e = new InaccessibleFundsException();
+        }
 
         // credit the account
         $this->credit("Payment received for order {$orderNumber}", $credit, $fee);
 
-        // if we couldn't get a lock, then we're done.
-        if (!$locked) {
-            Craft::warning("Aborted funds transfer for {$this->developer->id}: could not acquire lock on funds", __METHOD__);
-            return $this;
+        // only attempt the transfer if we could get a lock
+        if (!$e) {
+            // figure out how much we're going to transfer (credit amount minus fee, but no more than whatever they actually have in their account)
+            $balance = $this->getBalance();
+            $transferAmount = min($credit - $fee, $balance);
+
+            // don't transfer if they're in the red
+            if ($transferAmount <= 0) {
+                $e = new InsufficientFundsException($balance);
+            } else {
+                try {
+                    $this->_transferFunds("Funds transferred for order {$orderNumber}", $transferAmount, ['source_transaction' => $chargeId]);
+                } catch (StripeError $e) {
+                    // Something unexpected happened on Stripe's end
+                    Craft::$app->getErrorHandler()->logException($e);
+                } catch (\Throwable $e) {
+                }
+            }
         }
 
-        // attempt to transfer the funds
-        $transferAmount = $credit - $fee;
-        if ($this->_createTransfer($transferAmount, ['source_transaction' => $chargeId])) {
-            $this->debit("Funds transferred for order {$orderNumber}", $transferAmount);
-        };
+        if ($e) {
+            $this->_addTransaction("Failed transfer: {$e->getMessage()}");
+        }
 
         // unlock the funds
-        $mutex->release($lockName);
+        $this->_unlockFunds();
+    }
 
-        return $this;
+    /**
+     * Locks the developer's funds so no other FundsManager instance can deduct funds.
+     *
+     * @param int $timeout
+     * @return bool
+     */
+    private function _lockFunds(int $timeout = 0): bool
+    {
+        if ($this->_lockLevel === 0) {
+            if (!Craft::$app->getMutex()->acquire($this->_lockName, $timeout)) {
+                return false;
+            }
+        }
+        $this->_lockLevel++;
+        return true;
+    }
+
+    /**
+     * Unlocks the developer's funds so other FundsManager instances can deduct funds again.
+     */
+    private function _unlockFunds()
+    {
+        if ($this->_lockLevel === 0) {
+            return;
+        }
+        $this->_lockLevel--;
+        if ($this->_lockLevel === 0) {
+            Craft::$app->getMutex()->release($this->_lockName);
+        }
+    }
+
+    /**
+     * Attempts to transfer funds to the developer's Stripe account.
+     *
+     * @param string $note
+     * @param float $amount
+     * @param array $params
+     * @throws InvalidArgumentException if $credit or $fee are negative or 0
+     * @throws MissingStripeAccountException if the developer doesn't have a Stripe account
+     * @throws InaccessibleFundsException if the developer's funds could not be locked
+     * @throws InsufficientFundsException if the developer doesn't have enough funds for the transfer
+     * @throws StripeError if anything goes wrong on Stripe's end
+     */
+    private function _transferFunds(string $note, float $amount, array $params = [])
+    {
+        if ($amount <= 0) {
+            throw new InvalidArgumentException('Invalid transfer amount: '.$amount);
+        }
+
+        if (($stripeAccount = $this->developer->stripeAccount) === null) {
+            throw new MissingStripeAccountException();
+        }
+
+        // no double-dipping
+        if (!$this->_lockFunds()) {
+            throw new InaccessibleFundsException();
+        }
+
+        // make sure the funds are there
+        $balance = $this->getBalance();
+        if ($balance < $amount) {
+            $this->_unlockFunds();
+            throw new InsufficientFundsException($balance);
+        }
+
+        Stripe::setApiKey(getenv('STRIPE_API_KEY'));
+
+        $params += [
+            'amount' => round($amount * 100),
+            'currency' => 'usd',
+            'destination' => $stripeAccount,
+        ];
+
+        try {
+            Transfer::create($params);
+        } catch (StripeError $e) {
+            $this->_unlockFunds();
+            throw $e;
+        }
+
+        $this->debit($note, $amount);
+        $this->_unlockFunds();
     }
 
     /**
@@ -151,21 +249,25 @@ class FundsManager extends BaseObject
 
         if ($credit !== null) {
             $operator = '+';
-            $adjustment = $credit - ($fee  ?? 0);
-        } else {
+            $adjustment = $credit - ($fee ?? 0);
+        } else if ($debit !== null) {
             $operator = '-';
             $adjustment = $debit;
+        } else {
+            $adjustment = false;
         }
 
-        $db->createCommand()
-            ->update('craftcom_developers',
-                [
-                    'balance' => new Expression("[[balance]] {$operator} :adjustment", [':adjustment' => $adjustment])
-                ],
-                [
-                    'id' => $this->developer->id
-                ], [], false)
-            ->execute();
+        if ($adjustment !== false) {
+            $db->createCommand()
+                ->update('craftcom_developers',
+                    [
+                        'balance' => new Expression("[[balance]] {$operator} :adjustment", [':adjustment' => $adjustment])
+                    ],
+                    [
+                        'id' => $this->developer->id
+                    ], [], false)
+                ->execute();
+        }
 
         $ledgerSql = <<<SQL
 insert into {{craftcom_developerledger}} (
@@ -199,37 +301,5 @@ SQL;
             'fee' => $fee,
             'dateCreated' => Db::prepareDateForDb(new \DateTime()),
         ])->execute();
-    }
-
-    /**
-     * Attempts to transfer funds to the developer's Stripe account.
-     *
-     * @param float $amount
-     * @param array $params
-     * @return bool whether the transfer was successful
-     */
-    private function _createTransfer(float $amount, array $params = []): bool
-    {
-        if (($stripeAccount = $this->developer->stripeAccount) === null) {
-            Craft::warning("Aborted funds transfer for {$this->developer->id}: developer has no Stripe account", __METHOD__);
-            return false;
-        }
-
-        Stripe::setApiKey(getenv('STRIPE_API_KEY'));
-
-        $params += [
-            'amount' => round($amount * 100),
-            'currency' => 'usd',
-            'destination' => $stripeAccount,
-        ];
-
-        try {
-            Transfer::create($params);
-        } catch (\Throwable $e) {
-            Craft::warning("Unsuccessful funds transfer for {$this->developer->id}: {$e->getMessage()}", __METHOD__);
-            return false;
-        }
-
-        return true;
     }
 }
