f880e342d53ab51b63d075ee28661b343433d7c6
brandonkelly <brandon@pixelandtonic.com>
2018-03-18 04:55:10 -0700

    Include epilog table schemas (no data) in backups

--- config/general.php
+++ config/general.php
@@ -16,6 +16,29 @@ return [
         'csrfTokenName' => 'CRAFTCOM_CSRF_TOKEN',
         'phpSessionName' => 'CraftComSessionId',
         'generateTransformsBeforePageLoad' => true,
+        'backupCommand' => 'PGPASSWORD="{password}" ' .
+            'pg_dump ' .
+            '--dbname={database} '.
+            '--host={server} '.
+            '--port={port} '.
+            '--username={user} '.
+            '--if-exists '.
+            '--clean '.
+            '--file="{file}" '.
+            '--schema={schema} '.
+            '--schema=apilog '.
+            '--exclude-table-data \'{schema}.assetindexdata\' '.
+            '--exclude-table-data \'{schema}.assettransformindex\' '.
+            '--exclude-table-data \'{schema}.cache\' '.
+            '--exclude-table-data \'{schema}.sessions\' '.
+            '--exclude-table-data \'{schema}.templatecaches\' '.
+            '--exclude-table-data \'{schema}.templatecachecriteria\' '.
+            '--exclude-table-data \'{schema}.templatecacheelements\' ' .
+            '--exclude-table-data \'apilog.logs\' ' .
+            '--exclude-table-data \'apilog.request_cmslicenses\' ' .
+            '--exclude-table-data \'apilog.request_errors\' ' .
+            '--exclude-table-data \'apilog.request_pluginlicenses\' ' .
+            '--exclude-table-data \'apilog.requests\'',
     ],
     'prod' => [
         'allowUpdates' => false,
