cdcea7181a245c03e54e69cb0e57a8b34630c19e
andris-sevcenko <asevcenko@gmail.com>
2018-03-25 16:28:08 -0700

    Country list over API.

---
+++ src/controllers/api/v1/CountriesController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace craftnet\controllers\api\v1;
+
+use Craft;
+use craft\commerce\models\Country;
+use craft\commerce\models\State;
+use craft\commerce\Plugin as Commerce;
+use craftnet\controllers\api\BaseApiController;
+use yii\web\Response;
+
+/**
+ * Class CountriesController
+ */
+class CountriesController extends BaseApiController
+{
+
+    const COUNTRY_CACHE_KEY = 'countryListData';
+
+    /**
+     * Handles /v1/countries requests.
+     *
+     * @return Response
+     */
+    public function actionIndex(): Response
+    {
+        $countryList = $this->_getCountryList();
+
+        return $this->asJson([
+            'countries' => $countryList
+        ]);
+    }
+
+    /**
+     * Return a country list populated with state info.
+     *
+     * @return array
+     */
+    private function _getCountryList(): array
+    {
+        $cache = Craft::$app->getCache();
+
+        if ($cache->exists(self::COUNTRY_CACHE_KEY)) {
+            return $cache->get(self::COUNTRY_CACHE_KEY);
+        }
+
+        $commerce = Commerce::getInstance();
+
+        $countries = $commerce->getCountries()->getAllCountries();
+        $states = $commerce->getStates()->getAllStates();
+
+        $sortedStates = [];
+
+        /** @var State $state */
+        foreach ($states as $state) {
+            if (!array_key_exists($state->countryId, $sortedStates)) {
+                $sortedStates[$state->countryId] = [];
+            }
+
+            $sortedStates[$state->countryId][$state->abbreviation] = $state->name;
+        }
+
+        $countryList = [];
+
+        /** @var Country $country */
+        foreach ($countries as $country) {
+            $countryData = [
+                'name' => $country->name,
+                'stateRequired' => (bool)$country->isStateRequired
+            ];
+
+            if (array_key_exists($country->id, $sortedStates)) {
+                $countryData['states'] = $sortedStates[$country->id];
+            }
+
+            $countryList[$country->iso] = $countryData;
+        }
+
+        $cache->set(self::COUNTRY_CACHE_KEY, $countryList);
+
+        return $countryList;
+    }
+}
