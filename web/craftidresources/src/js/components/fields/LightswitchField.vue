35d01c4788a75655a7c60eee3ad0c05148206c96
Benjamin David <ben@dukt.net>
2018-04-20 15:43:06 +0200

    Added LightswitchField

---
+++ web/craftidresources/src/js/components/fields/LightswitchField.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="form-group">
+        <label v-if="label" :for="id">{{ label }}</label>
+        <div v-if="instructions" class="instructions">
+            <p>{{ instructions }}</p>
+        </div>
+        <lightswitch-input :id="id" :checked="checked" @change="$emit('change', $event)" @update:checked="$emit('update:checked', $event)" />
+    </div>
+</template>
+
+<script>
+    import LightswitchInput from '../inputs/LightswitchInput';
+
+    export default {
+
+        props: ['label', 'id', 'checked', 'instructions'],
+
+        components: {
+            LightswitchInput,
+        },
+
+    }
+</script>
