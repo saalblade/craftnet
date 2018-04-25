51e2c6eddc3a6e953376cf3c6f043c9cc5027abd
Benjamin David <ben@dukt.net>
2018-04-25 18:02:06 +0200

    Wording

--- web/craftidresources/src/js/components/LicenseRenewAlert.vue
+++ web/craftidresources/src/js/components/LicenseRenewAlert.vue
@@ -1,10 +1,10 @@
 <template>
     <div v-if="renewLicensesTotal > 0" class="alert alert-info" role="alert">
         <template v-if="renewLicensesTotal === 1">
-            {{ renewLicensesTotal }} of your licenses is about to expire. Renew it now to keep getting updates.
+            {{ renewLicensesTotal }} license is about to expire. Renew it now to keep getting updates.
         </template>
         <template v-else>
-            {{ renewLicensesTotal }} of your licenses are about to expire. Renew them now to keep getting updates.
+            {{ renewLicensesTotal }} licenses are about to expire. Renew them now to keep getting updates.
         </template>
     </div>
 </template>
