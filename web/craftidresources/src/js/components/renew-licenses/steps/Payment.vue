349771ad98886f615422a3180a96e2055bd04420
Benjamin David <ben@dukt.net>
2018-05-09 20:35:47 +0200

    Hook up billing fields

--- web/craftidresources/src/js/components/renew-licenses/steps/Payment.vue
+++ web/craftidresources/src/js/components/renew-licenses/steps/Payment.vue
@@ -21,19 +21,19 @@
             </div>
             <div class="md:w-1/2 px-4">
                 <h6>Billing Informations</h6>
-                <text-field placeholder="First Name" id="first-name" />
-                <text-field placeholder="Last Name" id="last-name" />
-                <text-field placeholder="Business Name" id="business-name" />
-                <text-field placeholder="Business Tax ID" id="business-tax-id" />
-                <text-field placeholder="Address 1" id="address-1" />
-                <text-field placeholder="Address 2" id="address-2" />
+                <text-field placeholder="First Name" id="first-name" v-model="billingInfo.firstName" :errors="errors['billingAddress.firstName']" />
+                <text-field placeholder="Last Name" id="last-name" v-model="billingInfo.lastName" :errors="errors['billingAddress.lastName']" />
+                <text-field placeholder="Business Name" id="business-name" v-model="billingInfo.businessName" :errors="errors['billingAddress.businessName']" />
+                <text-field placeholder="Business Tax ID" id="business-tax-id" v-model="billingInfo.businessTaxId" :errors="errors['billingAddress.businessTaxId']" />
+                <text-field placeholder="Address 1" id="address-1" v-model="billingInfo.address1" :errors="errors['billingAddress.address1']" />
+                <text-field placeholder="Address 2" id="address-2" v-model="billingInfo.address2" :errors="errors['billingAddress.address2']" />
 
                 <div class="md:flex -mx-2">
                     <div class="md:w-1/2 px-2">
-                        <text-field placeholder="City" id="city" />
+                        <text-field placeholder="City" id="city" v-model="billingInfo.city" :errors="errors['billingAddress.city']" />
                     </div>
                     <div class="md:w-1/2 px-2">
-                        <text-field placeholder="Zip Code" id="zip-code" />
+                        <text-field placeholder="Zip Code" id="zip-code" v-model="billingInfo.zipCode" :errors="errors['billingAddress.zipCode']" />
                     </div>
                 </div>
                 <div class="md:flex -mx-2">
