6f2966ad598ef5e41a62919b3c7caa55905c574d
Benjamin David <ben@dukt.net>
2018-06-14 02:05:20 +0200

    Currency filter

--- web/pluginsresources/src/plugins/filters.js
+++ web/pluginsresources/src/plugins/filters.js
@@ -1,7 +1,19 @@
 import Vue from 'vue'
+import Accounting from 'accounting'
 
-Vue.filter('currency', val => {
-    return val;
+/**
+ * Formats a value as a currency value.
+ */
+Vue.filter('currency', value => {
+    let precision = 2;
+    let floatValue = parseFloat(value);
+
+    // Auto precision
+    if(Math.round(floatValue) === floatValue) {
+        precision = 0;
+    }
+
+    return Accounting.formatMoney(floatValue, '$', precision);
 })
 
 Vue.filter('t', val => {
