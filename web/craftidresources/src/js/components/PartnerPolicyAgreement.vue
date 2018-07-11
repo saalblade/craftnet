c30a666113e6caa4f76bc21fa300fa7b06c9ba9a
Sam Hernandez <sam.a.hernandez@gmail.com>
2018-07-11 16:43:12 -0500

    Partner: Implement acceptsPartnerPolicy

---
+++ web/craftidresources/src/js/components/PartnerPolicyAgreement.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <div class="card mb-4">
+            <div class="card-body">
+                <p>Links to privacy policy, terms, etc. here</p>
+                <form @submit.prevent="onSubmit">
+                    <div class="form-group">
+                        <label><input type="checkbox" v-model="userDraft.acceptsPartnerPolicy"> I have read
+                            and all policies and consent to terms</label>
+                    </div>
+                    <input type="submit" class="btn btn-primary" :disabled="!userDraft.acceptsPartnerPolicy" value="Continue">
+                    <div v-if="loading" class="spinner"></div>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {mapState} from 'vuex'
+
+    export default {
+        data() {
+            return {
+                loading: false,
+                userDraft: {},
+            }
+        },
+
+        computed: {
+            ...mapState({
+                currentUser: state => state.account.currentUser,
+            }),
+        },
+
+        methods: {
+            onSubmit() {
+                if (!this.userDraft.acceptsPartnerPolicy) {
+                    return
+                }
+
+                this.loading = true
+
+                this.$store.dispatch('saveUser', this.userDraft).then(response => {
+                    this.loading = false;
+                }).catch(response => {
+                    this.loading = false;
+
+                    console.log(response)
+
+                    const errorMessage = response.data && response.data.error ? response.data.error : 'Couldn’t save settings.';
+                    this.$root.displayError(errorMessage);
+                });
+            }
+        },
+
+        mounted() {
+            this.userDraft = JSON.parse(JSON.stringify(this.currentUser));
+        }
+    }
+</script>
--- web/craftidresources/src/js/pages/PartnerOverview.vue
+++ web/craftidresources/src/js/pages/PartnerOverview.vue
@@ -2,47 +2,38 @@
 	<div>
 		<h1>Partner Overview</h1>
 
-        <div v-if="!partnerConsent">
-            <p>Links to privacy policy, terms, etc. here</p>
-            <p>
-                <label><input type="checkbox" v-model="partnerConsent"> I have read
-                    and all policies and consent</label>
-            </p>
-        </div>
-        <div v-else>
-            <p>Overview of Profile here.</p>
-            <p>Profile completion progress or badges for:</p>
-            <ul>
-                <li>Craft Verified</li>
-                <li>Craft Commerce Verified</li>
-                <li>Enterprise Verified</li>
-            </ul>
+        <partner-policy-agreement v-if="!currentUser.acceptsPartnerPolicy"></partner-policy-agreement>
+
+        <div v-else class="card mb-4">
+            <div class="card-body">
+                <p class="mb-4">Overview of Profile here.</p>
+                <p>Profile completion progress or badges for:</p>
+                <ul>
+                    <li>Craft Verified</li>
+                    <li>Craft Commerce Verified</li>
+                    <li>Enterprise Verified</li>
+                </ul>
+            </div>
         </div>
 	</div>
 </template>
 
 <script>
     import {mapState} from 'vuex'
+    import PartnerPolicyAgreement from '../components/PartnerPolicyAgreement'
 
     export default {
 
-        data() {
-            return {
-                partnerConsent: false,
-            }
-        },
+        data() {},
 
-        components: {},
+        components: {
+            PartnerPolicyAgreement
+        },
 
         computed: {
-          ...mapState({
-            hasApiToken: state => state.developers.hasApiToken,
-            currentUser: state => state.account.currentUser,
-          }),
+            ...mapState({
+                currentUser: state => state.account.currentUser,
+            }),
         },
-
-        methods: {},
-
-        mounted() {}
     }
 </script>
