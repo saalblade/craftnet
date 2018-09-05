70530b4d1fd58ddb83fda47c8d694cd5dec76e71
Benjamin David <ben@dukt.net>
2018-09-05 20:16:48 +0200

    Refactored FontAwesome imports

--- web/craftnetresources/id/src/js/app.js
+++ web/craftnetresources/id/src/js/app.js
@@ -7,23 +7,11 @@ import {formatCmsLicense, formatPluginLicense} from './filters/licenses';
 import App from './App.vue';
 
 // Font Awesome
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCoffee, faTimes, faTh, faBars, faPlus, faKey, faPlug, faImage, faUser, faPencilAlt, faExclamationTriangle, faBug } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-library.add([
-    faCoffee,
-    faTimes,
-    faTh,
-    faBars,
-    faPlus,
-    faKey,
-    faPlug,
-    faImage,
-    faUser,
-    faPencilAlt,
-    faExclamationTriangle,
-    faBug,
-])
+library.add([faCoffee, faTimes, faTh, faBars, faPlus, faKey, faPlug, faImage, faUser, faPencilAlt, faExclamationTriangle, faBug])
+
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
