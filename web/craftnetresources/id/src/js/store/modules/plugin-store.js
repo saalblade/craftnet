425234c7197e7aec0228b9c3da817cbe0590bf88
Benjamin David <ben@dukt.net>
2018-09-13 10:23:13 +0200

    Clean up

--- web/craftnetresources/id/src/js/store/modules/plugin-store.js
+++ web/craftnetresources/id/src/js/store/modules/plugin-store.js
@@ -32,11 +32,7 @@ const getters = {
 const actions = {
 
     getPluginStoreData({commit}) {
-        return axios.get('https://api.craftcms.test/v1/plugin-store', '', {
-                headers: {
-                    'X-CSRF-Token': Craft.csrfTokenValue,
-                }
-            })
+        return axios.get('https://api.craftcms.test/v1/plugin-store')
             .then(response => {
                 commit('updatePluginStoreData', {response})
             })
