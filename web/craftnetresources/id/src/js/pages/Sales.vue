5236c5ac3a9614c7c17ef7bc99cae9dd9969a149
Benjamin David <ben@dukt.net>
2018-10-06 12:25:03 +0200

    Clean up

--- web/craftnetresources/id/src/js/pages/Sales.vue
+++ web/craftnetresources/id/src/js/pages/Sales.vue
@@ -3,7 +3,7 @@
         <stripe-account-alert></stripe-account-alert>
 
         <h1>Sales</h1>
-        
+
         <template v-if="salesToRender.length > 0">
             <div class="field">
                 <text-input class="form-control" id="searchQuery" name="searchQuery" type="text" placeholder="Search sales" v-model="searchQuery" />
