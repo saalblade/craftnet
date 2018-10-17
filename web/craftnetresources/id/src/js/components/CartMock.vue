0ddd81bea25426cb3ed780d87af61919ffaefdb1
Benjamin David <ben@dukt.net>
2018-10-17 11:25:30 +0200

    Improved CartMock component

--- web/craftnetresources/id/src/js/components/CartMock.vue
+++ web/craftnetresources/id/src/js/components/CartMock.vue
@@ -1,26 +1,27 @@
 <template>
     <tbody v-if="mockCart.items.length > 0">
         <tr>
-            <th colspan="5">Renewals</th>
+            <th colspan="5"><div class="text-xl">Renewals</div></th>
         </tr>
 
         <tr v-for="(item, itemKey) in mockCart.items">
             <td></td>
-            <td colspan="2">Renewal</td>
-            <td>{{item.lineItem.total|currency}}</td>
-            <td class="text-right"><a @click="removeFromCartMock(itemKey)"><font-awesome-icon icon="times" /></a></td>
+            <td colspan="3">Renewal</td>
+            <td class="text-right">
+                <strong class="block text-xl">{{ item.lineItem.total|currency }}</strong>
+                <a @click="removeFromCartMock(itemKey)">Remove</a>
+            </td>
         </tr>
 
         <tr>
-            <th colspan="3" class="text-right">Renewal Total</th>
-            <th>{{cartTotal|currency}}</th>
-            <td class="thin"></td>
+            <th colspan="4" class="text-right text-xl">Renewal Total</th>
+            <th class="text-right text-xl">{{cartTotal|currency}}</th>
         </tr>
     </tbody>
 </template>
 
 <script>
-    import {mapState, mapGetters} from 'vuex'
+    import {mapState, mapGetters, mapActions} from 'vuex'
 
     export default {
 
@@ -40,6 +41,14 @@
                 cartTotal: 'cart/cartTotal',
             }),
         },
+
+        methods: {
+
+            ...mapActions({
+                removeFromCartMock: 'cart/removeFromCartMock',
+            })
+
+        }
 
     }
 </script>
