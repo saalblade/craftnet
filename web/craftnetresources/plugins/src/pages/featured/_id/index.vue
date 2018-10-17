cadb93a8bc0ac3d140d7ab68adaf93a47d98ccdc
Benjamin David <ben@dukt.net>
2018-10-17 22:28:43 +0200

    Fixed featured plugins’ styles

--- web/craftnetresources/plugins/src/pages/featured/_id/index.vue
+++ web/craftnetresources/plugins/src/pages/featured/_id/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-if="featuredPlugin" class="xcontainer">
+    <div v-if="featuredPlugin" class="xcontainer py-6">
         <h1>{{ featuredPlugin.title }}</h1>
         <plugin-grid :columns="4" :plugins="getPluginsByIds(featuredPlugin.plugins)"></plugin-grid>
     </div>
