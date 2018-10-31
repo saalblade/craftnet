df313627cef013872d325e9605ebf29fafff7fe2
Sam Hernandez <sam.a.hernandez@gmail.com>
2018-10-31 14:36:32 -0500

    Partner - Improves cloning helper

--- web/craftidresources/src/js/mixins/helpers.js
+++ web/craftidresources/src/js/mixins/helpers.js
@@ -1,6 +1,57 @@
+import get from 'lodash/get'
+import update from 'lodash/update'
+
 export default {
-    simpleClone(obj) {
-        return JSON.parse(JSON.stringify(obj))
+    methods: {
+        /**
+         * Clones an object without references or bindings.
+         * Optionally accepts a filtered property list with dot-syntax
+         * for nested properties.
+         *
+         * Example:
+         * ```
+         * let obj = {
+         *     test: 'value',
+         *     foo: {
+         *         bar: {
+         *             baz: 'one',
+         *             boo: 'two'
+         *         }
+         *     }
+         * }
+         *
+         * // an existing value and a missing value, with default
+         * let clone = simpleClone(obj, [
+         *     'foo.bar.baz',
+         *     ['aList', []]
+         * ])
+         *
+         * clone == {foo: {bar: {baz: 'hello'}}, aList: []} // true
+         * ```
+         *
+         * @param {Object} obj
+         * @param {Array} propertyList
+         */
+        simpleClone(obj, propertyList) {
+            let clone = JSON.parse(JSON.stringify(obj))
+
+            if (!propertyList) {
+                return clone
+            }
+
+            let filteredClone = {}
+
+            for (let i = 0; i < propertyList.length; i++) {
+                const path = propertyList[i];
+
+                if (typeof path === 'object') {
+                    update(filteredClone, path, () => get(clone, path[0], path[1]))
+                } else {
+                    update(filteredClone, path, () => get(clone, path, null))
+                }
+            }
+
+            return filteredClone;
+        }
     }
 }
-
