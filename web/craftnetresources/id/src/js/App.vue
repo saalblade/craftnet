b493030c5135c1483e5c6801d2e06511f9b03596
Benjamin David <ben@dukt.net>
2018-11-01 11:32:39 +0100

    Fixed dispatch issues

--- web/craftnetresources/id/src/js/App.vue
+++ web/craftnetresources/id/src/js/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app">
         <auth-manager ref="authManager"></auth-manager>
-        <renew-licenses-modal v-if="showRenewLicensesModal" :license="renewLicense" @cancel="$dispatch('setShowRenewLicensesModal', false)" />
+        <renew-licenses-modal v-if="showRenewLicensesModal" :license="renewLicense" @cancel="$store.dispatch('setShowRenewLicensesModal', false)" />
 
         <template v-if="notification">
             <div id="notifications-wrapper" :class="{'hide': !notification }">
