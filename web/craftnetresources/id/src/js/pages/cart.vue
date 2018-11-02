03c25efced454157fd64af3a407d84d9520b0ed9
Benjamin David <ben@dukt.net>
2018-11-02 17:38:30 +0100

    Implemented Update Until and Quantity dropdowns to the cart

--- web/craftnetresources/id/src/js/pages/cart.vue
+++ web/craftnetresources/id/src/js/pages/cart.vue
@@ -46,17 +46,17 @@
                                 <select-field v-model="itemUpdates[itemKey]" :options="itemUpdateOptions(itemKey)" />
                             </td>
                             <td>
-                                <select-field value="1" :options="quantityOptions"></select-field>
+                                <select-field v-model="itemQuantity[itemKey]" :options="quantityOptions"></select-field>
                             </td>
                             <td class="text-right">
-                                <strong class="block text-xl">{{ item.lineItem.total|currency }}</strong>
+                                <strong class="block text-xl">{{ itemTotal(itemKey)|currency }}</strong>
                                 <a @click="removeFromCart(itemKey)">Remove</a>
                             </td>
                         </tr>
 
                         <tr>
                             <th class="text-right text-xl" colspan="4">Total</th>
-                            <td class="text-right text-xl"><strong>{{ cart.totalPrice|currency }}</strong></td>
+                            <td class="text-right text-xl"><strong>{{ total|currency }}</strong></td>
                         </tr>
                         </tbody>
 
@@ -98,6 +98,7 @@
             return {
                 loading: false,
                 itemUpdates: {},
+                itemQuantity: {},
             }
         },
 
@@ -111,8 +112,7 @@
                 cartItems: 'cart/cartItems',
             }),
 
-            quantityOptions()
-            {
+            quantityOptions() {
                 return [
                     {label: 1, value: 1},
                     {label: 2, value: 2},
@@ -120,7 +120,18 @@
                     {label: 4, value: 4},
                     {label: 5, value: 5},
                 ]
+            },
+
+            total() {
+                let total = 0
+
+                this.cartItems.forEach(function(item, key) {
+                    total += this.itemTotal(key)
+                }.bind(this))
+
+                return total
             }
+
         },
 
         methods: {
@@ -138,13 +149,49 @@
             },
 
             itemUpdateOptions(itemKey) {
-                return [
-                    {
-                        label: 'Updates Until 9/28/2019 (Free)',
-                        value: '',
+                const item = this.cartItems[itemKey]
+                const renewalPrice = item.lineItem.purchasable.renewalPrice
+                const itemUpdate = this.itemUpdates[itemKey]
+                const years = 5
+                let options = []
+
+                for (let i = 1; i <= years; i++) {
+                    const currentDate = new Date()
+                    const year = currentDate.getFullYear() + i
+                    const month = currentDate.getMonth()
+                    const day = currentDate.getDay()
+                    const date = new Date(year, month, day)
+                    const price = renewalPrice * (i - itemUpdate);
+
+                    let label = "Updates Until " + this.$options.filters.moment(date, 'L')
+
+                    if (price !== 0) {
+                        let sign = '';
+
+                        if (price > 0) {
+                            sign = '+';
+                        }
+
+                        label += " (" + sign + this.$options.filters.currency(price) + ")"
                     }
-                ]
+
+                    options.push({
+                        label: label,
+                        value: i,
+                    })
+                }
+
+                return options
             },
+
+            itemTotal(itemKey) {
+                const purchasable = this.cartItems[itemKey].lineItem.purchasable
+                const price = parseInt(purchasable.price)
+                const renewalsTotal = parseInt(purchasable.renewalPrice) * (this.itemUpdates[itemKey] - 1)
+                const quantity = this.itemQuantity[itemKey]
+
+                return (price + renewalsTotal) * quantity
+            }
 
         },
 
@@ -156,6 +203,11 @@
                     this.getCart()
                         .then(() => {
                             this.loading = false
+
+                            this.cartItems.forEach(function(item, key) {
+                                this.$set(this.itemUpdates, key, 1)
+                                this.$set(this.itemQuantity, key, 1)
+                            }.bind(this))
                         })
                         .catch(() => {
                             this.loading = false
