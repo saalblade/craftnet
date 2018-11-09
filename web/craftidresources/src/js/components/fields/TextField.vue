6118a30e699a32e746c7963fa28e0f20b5f5c894
Sam Hernandez <sam.a.hernandez@gmail.com>
2018-11-09 15:39:40 -0600

    Character count on text fields with max

--- web/craftidresources/src/js/components/fields/TextField.vue
+++ web/craftidresources/src/js/components/fields/TextField.vue
@@ -6,19 +6,33 @@
 			<p>{{ instructions }}</p>
 		</div>
 
-		<text-input
-			:autofocus="autofocus"
-			:class="{'is-invalid': errors }"
-			:disabled="disabled"
-			:id="id"
-			:placeholder="placeholder"
-			:value="value"
-			:mask="mask"
-			@input="$emit('input', $event)"
-			:autocapitalize="autocapitalize"
-			:spellcheck="spellcheck"
-			:readonly="readonly"
-			ref="input"/>
+		<div class="relative" :class="{'mt-4': max}">
+			<div v-if="max"
+				 class="text-xs text-right pr-1 absolute" style="right: 0; top: -1rem;"
+				 :class="{
+					'text-grey': remainingChars >= 10,
+					'text-orange': remainingChars < 10 && remainingChars >= 0,
+					'text-red': remainingChars < 0
+				}">{{ remainingChars }}</div>
+
+			<text-input
+					:autofocus="autofocus"
+					:class="{
+						'is-invalid': errors,
+						'text-red-dark': max && max < this.value.length
+					}"
+					:disabled="disabled"
+					:id="id"
+					:placeholder="placeholder"
+					:value="value"
+					:mask="mask"
+					@input="$emit('input', $event)"
+					:autocapitalize="autocapitalize"
+					:spellcheck="spellcheck"
+					:readonly="readonly"
+					ref="input"/>
+		</div>
+
 
 		<div class="invalid-feedback" v-for="error in errors">{{ error }}</div>
 	</div>
@@ -29,7 +43,7 @@
 
     export default {
 
-        props: ['label', 'id', 'placeholder', 'value', 'autofocus', 'errors', 'disabled', 'instructions', 'mask', 'autocapitalize', 'spellcheck', 'readonly'],
+        props: ['label', 'id', 'placeholder', 'value', 'autofocus', 'errors', 'disabled', 'instructions', 'mask', 'autocapitalize', 'spellcheck', 'readonly', 'max'],
 
         components: {
             TextInput,
@@ -39,7 +53,15 @@
             this.$on('focus', function(msg) {
                 this.$refs.input.$emit('focus');
             })
-        }
+        },
+
+		computed: {
+            remainingChars() {
+                if (this.max) {
+                    return this.max - this.value.length
+				}
+			}
+		}
 
     }
 </script>
