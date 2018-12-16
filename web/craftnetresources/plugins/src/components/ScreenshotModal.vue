a2871818b5bcf77ab7265f25dc2b708fe077e54d
Benjamin David <ben@dukt.net>
2018-12-16 14:42:50 +0100

    Use the escape key to close the screenshot modal

--- web/craftnetresources/plugins/src/components/ScreenshotModal.vue
+++ web/craftnetresources/plugins/src/components/ScreenshotModal.vue
@@ -28,9 +28,24 @@
 
             close() {
                 this.$store.commit('app/toggleScreenshotModal')
+            },
+
+            handleEscapeKey(e) {
+                if (e.keyCode === 27) {
+                    this.close()
+                }
             }
 
+        },
+
+        created() {
+            window.addEventListener('keydown', this.handleEscapeKey)
+        },
+
+        beforeDestroy: function () {
+            window.removeEventListener('keydown', this.handleEscapeKey)
         }
+
 
     }
 </script>
