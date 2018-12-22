4655f2d385d0e061f701933a045d4c6ca5fa01db
brandonkelly <brandon@pixelandtonic.com>
2018-12-21 16:05:33 -0800

    UpgradeDiscount wasn't doing anything

--- src/Module.php
+++ src/Module.php
@@ -88,9 +88,6 @@ class Module extends \yii\base\Module
             $e->types[] = CmsEdition::class;
             $e->types[] = PluginEdition::class;
         });
-        Event::on(OrderAdjustments::class, OrderAdjustments::EVENT_REGISTER_ORDER_ADJUSTERS, function(RegisterComponentTypesEvent $e) {
-            $e->types[] = EditionUpgradeDiscount::class;
-        });
 
         // register our custom receipt system message
         Event::on(SystemMessages::class, SystemMessages::EVENT_REGISTER_MESSAGES, function(RegisterEmailMessagesEvent $e) {
