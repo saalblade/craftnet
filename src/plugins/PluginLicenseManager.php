c6fce0a397c779f9456ae15308a6b86220e50a7a
Benjamin David <ben@dukt.net>
2019-02-22 19:06:41 +0100

    Removed unused methods

--- src/plugins/PluginLicenseManager.php
+++ src/plugins/PluginLicenseManager.php
@@ -457,76 +457,8 @@ class PluginLicenseManager extends Component
         return $this->transformLicensesForOwner($results, $owner);
     }
 
-    public function getSalesByPluginOwner(User $owner, $searchQuery, $limit, $page)
-    {
-        $defaultLimit = 30;
-        $perPage = $limit ?? $defaultLimit;
-        $offset = ($page - 1) * $perPage;
-
-        $query = $this->_getSalesQuery($owner);
-
-        $query
-            ->offset($offset)
-            ->limit($limit);
-
-        $results = $query->all();
-
-        foreach ($results as &$row) {
-            $row['netAmount'] = number_format($row['grossAmount'] * 0.8, 2);
-            $row['plugin'] = [
-                'id' => $row['pluginId'],
-                'name' => $row['pluginName']
-            ];
-            $row['customer'] = [
-                'id' => $row['ownerId'],
-                'name' => implode(' ', array_filter([$row['ownerFirstName'], $row['ownerLastName']])),
-                'email' => $row['ownerEmail'] ?? $row['orderEmail'],
-            ];
-
-            unset($row['pluginId'], $row['pluginName'], $row['ownerId'], $row['ownerFirstName'], $row['ownerLastName'], $row['ownerEmail']);
-        }
-
-        return $results;
-    }
-
-    public function getTotalSalesByPluginOwner(User $owner)
-    {
-        $query = $this->_getSalesQuery($owner);
-
-        return $query->count();
-    }
-
     // Public Methods
     // =========================================================================
-
-    /**
-     * @param User $owner
-     * @return Query
-     */
-    private function _getSalesQuery(User $owner): Query
-    {
-        return (new Query())
-            ->select([
-                'lineitems.id AS id',
-                'plugins.id AS pluginId',
-                'plugins.name AS pluginName',
-                'lineitems.salePrice AS grossAmount',
-                'users.id AS ownerId',
-                'users.firstName AS ownerFirstName',
-                'users.lastName AS ownerLastName',
-                'users.email AS ownerEmail',
-                'lineitems.dateCreated AS saleTime',
-                'orders.email AS orderEmail',
-            ])
-            ->from(['craftnet_pluginlicenses_lineitems licenses_items'])
-            ->innerJoin('commerce_lineitems lineitems', '[[lineitems.id]] = [[licenses_items.lineItemId]]')
-            ->innerJoin('commerce_orders orders', '[[orders.id]] = [[lineitems.orderId]]')
-            ->innerJoin('craftnet_pluginlicenses licenses', '[[licenses.id]] = [[licenses_items.licenseId]]')
-            ->innerJoin('craftnet_plugins plugins', '[[plugins.id]] = [[licenses.pluginId]]')
-            ->leftJoin('users', '[[users.id]] = [[licenses.ownerId]]')
-            ->where(['plugins.developerId' => $owner->id])
-            ->orderBy(['lineitems.dateCreated' => SORT_DESC]);
-    }
 
     /**
      * Transforms licenses for the given owner.
