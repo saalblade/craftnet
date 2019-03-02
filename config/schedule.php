08b8851e68f0412e57c2d0643cc73b8a645b789c
brandonkelly <brandon@pixelandtonic.com>
2019-03-02 11:57:04 -0800

    Only run scheduled jobs on production

--- config/schedule.php
+++ config/schedule.php
@@ -1,5 +1,10 @@
 <?php
 
+// Only run scheduled jobs on production
+if (getenv('CRAFT_ENV') !== 'prod') {
+    return;
+}
+
 /** @var $schedule omnilight\scheduling\Schedule */
 
 $schedule->command('craftnet/licenses/send-reminders')
