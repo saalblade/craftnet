ab88b0ca15e2e6823af0a2cc7793da18b30e37bf
Benjamin David <ben@dukt.net>
2019-03-19 13:02:21 +0100

    Clean up unused code

--- web/craftnetresources/id/src/js/components/billing/InvoicesTable.vue
+++ web/craftnetresources/id/src/js/components/billing/InvoicesTable.vue
@@ -39,27 +39,6 @@
                 </template>
             </vuetable>
         </div>
-
-        <!--<div class="responsive-content">
-        <table class="table">
-            <thead>
-            <tr>
-                <th>ID</th>
-                <th>Price</th>
-                <th>Date</th>
-                <th>Receipt</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="(invoice, key) in invoices" :key="key">
-                <td><router-link :to="'/account/billing/invoices/' + invoice.number">{{ invoice.shortNumber }}</router-link></td>
-                <td>{{ invoice.totalPrice|currency }}</td>
-                <td><template v-if="invoice.datePaid">{{ invoice.datePaid.date|moment("L") }}</template></td>
-                <td><a :href="invoice.pdfUrl">Download Receipt</a></td>
-            </tr>
-            </tbody>
-        </table>
-    </div>-->
     </div>
 </template>
 
